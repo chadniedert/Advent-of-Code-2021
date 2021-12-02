019823713dfa610bf4a5462aefaeb4e96876ce41
Chad Niedert <chad.niedert@icloud.com>
2021-12-02 19:45:34 +0000

    completed day 2 star 1

--- Day2.java
+++ Day2.java
@@ -20,7 +20,35 @@ public class Day2
 		}
 		long start = System.nanoTime();
 
-		int
+		int horizPos = 0;
+		int vertPos = 0;
+		int prod = 0;
+
+		for (int i = 0; i < dataLines.size(); i++)
+		{
+			if (dataLines.get(i).contains("forward"))
+			{
+				horizPos += Integer.parseInt(dataLines.get(i).substring(8,9));
+			}
+			else if (dataLines.get(i).contains("up"))
+			{
+				vertPos -= Integer.parseInt(dataLines.get(i).substring(3,4));
+			}
+			else if (dataLines.get(i).contains("down"))
+			{
+				vertPos += Integer.parseInt(dataLines.get(i).substring(5,6));
+			}
+			else 
+			{
+				System.out.println("something went wrong");
+				break;
+			}
+		}
+
+		prod = horizPos * vertPos;
+		System.out.println("horizPos = " + horizPos);
+		System.out.println("vertPos = " + vertPos);
+		System.out.println("prod = " + prod);
 
 	
 		// keep this line at the end of your code
