d4df44d4cd45a43870a75d9f639a018a01a23e65
Chad Niedert <chad.niedert@icloud.com>
2021-12-02 20:03:30 +0000

    Completed Day 2 Star 2. Commented out Day 2 Star 1 solution.

--- Day2.java
+++ Day2.java
@@ -20,6 +20,7 @@ public class Day2
 		}
 		long start = System.nanoTime();
 
+		/* DAY 2 STAR 1 SOLUTION
 		int horizPos = 0;
 		int vertPos = 0;
 		int prod = 0;
@@ -49,6 +50,48 @@ public class Day2
 		System.out.println("horizPos = " + horizPos);
 		System.out.println("vertPos = " + vertPos);
 		System.out.println("prod = " + prod);
+
+		 * OUTPUT
+		 * horizPos = 1911
+     * vertPos = 724
+     * prod = 1383564 
+		 * Elapsed time: 88463891ns
+		 */
+
+		// DAY 2 STAR 2 SOLUTION
+		long horizPos = 0;
+		long vertPos = 0;
+		long prod = 0;
+		long aim = 0;
+
+		for (int i = 0; i < dataLines.size(); i++)
+		{
+			if (dataLines.get(i).contains("forward"))
+			{
+				horizPos += Integer.parseInt(dataLines.get(i).substring(8,9));
+				vertPos += aim * Integer.parseInt(dataLines.get(i).substring(8,9));
+				System.out.println("vertPos scaled and is now: " + vertPos);
+			}
+			else if (dataLines.get(i).contains("up"))
+			{
+				aim -= Integer.parseInt(dataLines.get(i).substring(3,4));
+			}
+			else if (dataLines.get(i).contains("down"))
+			{
+				aim += Integer.parseInt(dataLines.get(i).substring(5,6));
+			}
+			else 
+			{
+				System.out.println("something went wrong");
+				break;
+			}
+		}
+
+		prod = horizPos * vertPos;
+		System.out.println("horizPos = " + horizPos);
+		System.out.println("vertPos = " + vertPos);
+		System.out.println("prod = " + prod);
+
 
 	
 		// keep this line at the end of your code
