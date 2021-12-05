406801974cddb73d733b13a41018d9bd60a24c4b
Chad Niedert <chad.niedert@icloud.com>
2021-12-05 03:20:40 +0000

    Needed to update counts of each bit after each cycle through each index. Complete Day 3 Star 2.

--- Day3Star2.java
+++ Day3Star2.java
@@ -20,33 +20,34 @@ public class Day3Star2
 		}
 		long start = System.nanoTime();
 
-		String gammaRate = "", epsilonRate = ""; 
-		int powerCons = 0;
-
-		ArrayList<Integer> bit1Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
-		ArrayList<Integer> bit0Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
-		ArrayList<String> oxyGenList = new ArrayList<String>();
+		ArrayList<Integer> oxy_bit1Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
+		ArrayList<Integer> oxy_bit0Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
+		ArrayList<Integer> co2_bit1Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
+		ArrayList<Integer> co2_bit0Count = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0));
+		ArrayList<String> oxyList = new ArrayList<String>();
 		ArrayList<String> co2List = new ArrayList<String>();
 
 		// Build oxyGenList, co2List from dataLines
 		for (String num : dataLines)
 		{
-			oxyGenList.add(num);
+			oxyList.add(num);
 			co2List.add(num);
 		}
 
-		// Builds bit1Count and bit0Count
+		// Builds oxy_bit1Count, oxy_bit0Count, co2_bit1Count, co2_bit2Count
 		for (String num : dataLines)
 		{
 			for (int i = 0; i < num.length(); i++)
 			{
 				if (num.substring(i, i+1).equals("1"))
 				{
-					bit1Count.set(i, bit1Count.get(i) + 1);
+					oxy_bit1Count.set(i, oxy_bit1Count.get(i) + 1);
+					co2_bit1Count.set(i, co2_bit1Count.get(i) + 1);
 				}
 				else if (num.substring(i, i+1).equals("0"))
 				{
-					bit0Count.set(i, bit0Count.get(i) + 1);
+					oxy_bit0Count.set(i, oxy_bit0Count.get(i) + 1);
+					co2_bit0Count.set(i, co2_bit0Count.get(i) + 1);
 				}
 				else
 				{
@@ -56,80 +57,124 @@ public class Day3Star2
 			}
 		}
 
-		// Removes appropriate values from oxyGenList, co2List
+		// Removes appropriate values from oxyList
 		for (int i = 0; i < 12; i++)
 		{
-			if (bit1Count.get(i) >= bit0Count.get(i))
+			updateCounts(oxyList, oxy_bit1Count, oxy_bit0Count);
+			if (oxy_bit1Count.get(i) >= oxy_bit0Count.get(i))
 			{
-				for (int j = 0; j < oxyGenList.size(); j++)
+				for (int j = 0; j < oxyList.size(); j++)
 				{
-					if (oxyGenList.get(j).substring(i, i + 1).equals("0"))
+					if (oxyList.get(j).substring(i, i + 1).equals("0"))
 					{
-						if (oxyGenList.size() > 1)
+						if (oxyList.size() > 1)
 						{
-							oxyGenList.remove(j);
+							oxyList.remove(j);
 							j--;
 						}
 					}
 				}
-				for (int l = 0; l < co2List.size(); l++)
-				{
-					if (co2List.get(l).substring(i, i + 1).equals("1"))
-					{
-						if (co2List.size() > 1)
-						{
-							co2List.remove(l);
-							l--;
-						}
-					}
-				}	
 			}
 			else
 			{
-				for (int k = 0; k < oxyGenList.size(); k++)
+				for (int k = 0; k < oxyList.size(); k++)
 				{
-					if (oxyGenList.get(k).substring(i, i + 1).equals("1"))
+					if (oxyList.get(k).substring(i, i + 1).equals("1"))
 					{
-						if (oxyGenList.size() > 1)
+						if (oxyList.size() > 1)
 						{
-							oxyGenList.remove(k);
+							oxyList.remove(k);
 							k--;
 						}
 					}
 				}
-				for (int m = 0; m < co2List.size(); m++)
+			}
+		}
+
+		// Removes appropriate values from co2List
+		for (int i = 0; i < 12; i++)
+		{
+			updateCounts(co2List, co2_bit1Count, co2_bit0Count);
+			if (co2_bit1Count.get(i) < co2_bit0Count.get(i))
+			{
+				for (int j = 0; j < co2List.size(); j++)
 				{
-					if (co2List.get(m).substring(i, i + 1).equals("0"))
+					if (co2List.get(j).substring(i, i + 1).equals("0"))
 					{
 						if (co2List.size() > 1)
 						{
-							co2List.remove(m);
-							m--;
+							co2List.remove(j);
+							j--;
 						}
 					}
 				}
 			}
-			
+			else
+			{
+				for (int k = 0; k < co2List.size(); k++)
+				{
+					if (co2List.get(k).substring(i, i + 1).equals("1"))
+					{
+						if (co2List.size() > 1)
+						{
+							co2List.remove(k);
+							k--;
+						}
+					}
+				}
+			}
 		}
 
-		
-		System.out.println("oxyGenList: " + oxyGenList);
-		
-		int oxyGenRating = Integer.parseInt(oxyGenList.get(0), 2);
-		System.out.println("oxyGenRating = " + oxyGenRating);
-		System.out.println("oxyGenList.size() = " + oxyGenList.size());
+		System.out.println("oxyList: " + oxyList);
+		int oxyRating = Integer.parseInt(oxyList.get(0), 2);
+		System.out.println("oxyRating = " + oxyRating);
+		System.out.println("oxyList.size() = " + oxyList.size());
 
 		System.out.println("co2List: " + co2List);
-		
 		int co2Rating = Integer.parseInt(co2List.get(0), 2);
 		System.out.println("co2Rating = " + co2Rating);
 		System.out.println("co2List.size() = " + co2List.size());
 
-		System.out.println("generator rating: " + co2Rating * oxyGenRating);
-
+		int lifeSupportRating = oxyRating * co2Rating;
+		System.out.println("lifeSupportRating = " + lifeSupportRating);
+		
 		// keep this line at the end of your code
 		long end = System.nanoTime();
 		long elapsedTime = end - start;
 		System.out.format("Elapsed time: %dns%n", elapsedTime);
+	}
+
+	public static void updateCounts(ArrayList<String> storageArr, ArrayList<Integer> arr1, ArrayList<Integer> arr0)
+	{
+		// Reset arr1, arr2 to 0
+		for (int i = 0; i < arr1.size(); i++)
+		{
+			arr1.set(i, 0);
+		}
+		for (int i = 0; i < arr0.size(); i++)
+		{
+			arr0.set(i, 0);
+		}
+
+		for (String num : storageArr)
+		{
+			
+			for (int i = 0; i < num.length(); i++)
+			{
+				if (num.substring(i, i + 1).equals("1"))
+				{
+					arr1.set(i, arr1.get(i) + 1);
+				}
+				else if (num.substring(i, i+1).equals("0"))
+				{
+					arr0.set(i, arr0.get(i) + 1);
+				}
+				else
+				{
+					System.out.println("Something went wrong.");
+					break;
+				}
+			}
+		}
 	}
 }
